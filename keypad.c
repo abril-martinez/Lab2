ebd4d08309da1375a0a34653e2844e933a2590a3
abril-martinez <amartinez28@email.arizona.edu>
2014-10-17 06:44:38 -0700

    Updated KeypadScan function

--- keypad.c
+++ keypad.c
@@ -2,107 +2,87 @@
 
 #include "p24fj64ga002.h"
 #include "keypad.h"
-
+#include "lcd.h"
+//#include <stdio.h>
 
 // ******************************************************************************************* //
+
+//#define ROWA LATA
+//#define ROWB LATB
 
 #define KEY_C0			PORTBbits.RB2
 #define KEY_C1		    PORTBbits.RB3	
 #define KEY_C2			PORTBbits.RB5 
-#define KEY_R0			PORTBbits.RB11 
-#define KEY_R1			PORTBbits.RB10 
-#define KEY_R2			PORTAbits.RA0 
-#define KEY_R3			PORTAbits.RA1 
+#define KEY_R0			LATBbits.LATB11        
+#define KEY_R1			LATBbits.LATB10 
+#define KEY_R2			LATAbits.LATA0
+#define KEY_R3			LATAbits.LATA1
 
-/************************************Variables*************************************************/
+//char register[4]
 
-char returnChar = 'Z';
-
-/***********************************Fucntion Prototypes****************************************/
-void KeypadInitialize();
-void active_column(int col);
-char KeypadScan();
-char ScanRows();
-
-/***************************************Initialize*********************************************/
 void KeypadInitialize() {
+	
+ // Configure cols->inputs and rows->outputs
+ TRISBbits.TRISB2  = 1;
+ TRISBbits.TRISB3  = 1;
+ TRISBbits.TRISB5  = 1; 
+ TRISBbits.TRISB11 = 0;
+ TRISBbits.TRISB10 = 0;
+ TRISAbits.TRISA0  = 0;
+ TRISAbits.TRISA1  = 0;	
 
-AD1PCFGbits.PCFG4 = 1;
-AD1PCFGbits.PCFG5 = 1;
-AD1PCFGbits.PCFG0 = 1;
-AD1PCFGbits.PCFG1 = 1;
+ //Enable input pins for CN interrupt
+ CNEN1bits.CN6IE =  1; //RB2
+ CNEN1bits.CN7IE =  1; //RB3
+ CNEN2bits.CN27IE = 1; //RB5
 
-// Configure cols->inputs and rows->outputs
-TRISBbits.TRISB2  = 1;
-TRISBbits.TRISB3  = 1;
-TRISBbits.TRISB5  = 1; 
-TRISBbits.TRISB11 = 0;
-TRISBbits.TRISB10 = 0;
-TRISAbits.TRISA0  = 0;
-TRISAbits.TRISA1  = 0;
+ //Enable Change of Notification Interrupt for columns
+ IEC1bits.CNIE = 1; // Enable CN interrupt
+ IFS1bits.CNIF = 0; //clear flag
 
-// Enable the CNPU resistors for inputs
-CNPU1bits.CN6PUE =  1; //RB2
-CNPU1bits.CN7PUE =  1; //RB3
-CNPU2bits.CN27PUE = 1; //RB5
+ // Enable the CNPU resistors for inputs
+ CNPU1bits.CN6PUE =  1; //RB2
+ CNPU1bits.CN7PUE =  1; //RB3
+ CNPU2bits.CN27PUE = 1; //RB5
 
-//Enable input pins for CN interrupt
-CNEN1bits.CN6IE =  1; //RB2
-CNEN1bits.CN7IE =  1; //RB3
-CNEN2bits.CN27IE = 1; //RB5
+ //Configure ODC register bits to use open drain configuration for output pins
+ ODCBbits.ODB11 = 1;
+ ODCBbits.ODB10 = 1;
+ ODCAbits.ODA0 =  1;
+ ODCAbits.ODA1 =  1;
 
-//Enable Change of Notification Interrupt for columns
-IEC1bits.CNIE = 1; // Enable CN interrupt
-IFS1bits.CNIF = 0; //clear flag
+ // Ground all the rows
+ KEY_R0 = 0;
+ KEY_R1 = 0;
+ KEY_R2 = 0;
+ KEY_R3 = 0;
 
-//Configure ODC register bits to use open drain configuration for output pins
-ODCBbits.ODB11 = 1;
-ODCBbits.ODB10 = 1;
-ODCAbits.ODA0 =  1;
-ODCAbits.ODA1 =  1;
+ AD1PCFGbits.PCFG4 = 1;
+ AD1PCFGbits.PCFG5 = 1;
+ AD1PCFGbits.PCFG0 = 1;
+ AD1PCFGbits.PCFG1 = 1;
 
 //Initialize inputs pins to off
-LATBbits.LATB2 = 1;
-LATBbits.LATB3 = 1;
-LATBbits.LATB5 = 1;
+LATBbits.LATB2 = 0;
+LATBbits.LATB3 = 0;
+LATBbits.LATB5 = 0;
 
+ // Ground all rows
+ //ROWB &= 0xF3FF; // RB11 & RB10 = 0
+ //ROWA &= 0xFFFC; // RA0 & RA1 = 0
 }
 
-//**************************************Set Column Active**************************************//
+// ******************************************************************************************* //
 
-void active_column(int col) // set one column active
-  {
-   if(col == 0) 
-     {
-      LATBbits.LATB2 = 0;
-      LATBbits.LATB3 = 1;
-	  LATBbits.LATB5 = 1;
-     }
-    
-   else if(col == 1) 
-     {
-      LATBbits.LATB2 = 1;
-      LATBbits.LATB3 = 0;
-	  LATBbits.LATB5 = 1;
-     }
-    
-   else if(col == 2) 
-     {
-      LATBbits.LATB2 = 1;
-      LATBbits.LATB3 = 1;
-	  LATBbits.LATB5 = 0; 
-     }
-  }
+char KeypadScan() {
+	char key = -1;
+	int row=0;	
+	int col0 = 0; //Column zero flag
+	int col1 = 0; //Column one flag
+	int col2 = 0; //Column two flag
+	int rows = 0; //num of rows pressed
 
-// **************************************KeyPad Scan***************************************************** //
-
-char KeypadScan() 
-  { 
-    char key = -1;
-    key = ScanRows(); //read each row when a specific col is set active
- 
-
-    // TODO: Implement the keypad scanning procedure to detect if exactly one button of the 
+	// TODO: Implement the keypad scanning procedure to detect if exactly one button of the 
 	// keypad is pressed. The function should return:
 	//
 	//      -1         : Return -1 if no keys are pressed.
@@ -118,100 +98,125 @@ char KeypadScan()
 	//           is processed. This is to prevent invalid keypress from being processed if the 
 	//           users presses multiple keys simultaneously.
 	//
-	return key;
- }
 
-// ***********************************Scan Rows Function*************************************** //
+	while((KEY_C0 == 0) || (KEY_C1 == 0) || (KEY_C2 == 0)){
+		
+		//These three if statements keep track of how many columns have been activated
+		if (KEY_C0 == 0) 	col0 = 1; //If button in column zero was pressed set flag to high
+		if (KEY_C1 == 0) 	col1 = 1; //If button in column one was pressed set flag to high
+		if (KEY_C2 == 0) 	col2 = 1; //If button in column two was pressed set flag to high
 
-char ScanRows()
- {
-   int column=0;
-   for(column=0; column<3; column++)
-    {
-     active_column(column); // determines which column is active   
-   
-   
-     if(column == 0) 
-      {
-       if((KEY_R0 == 1))// && (KEY_R1 == 1) && (KEY_R2 == 1) && (KEY_R3 == 1)) 
-        {
-         returnChar = '1';
-       //  while(KEY_R0== 1); // wait for button release
-        }
-       else if(KEY_R1 == 0 && KEY_R0 == 1 && KEY_R2 == 1 && KEY_R3 ==1)
-        {
-         returnChar = '4';
-         while(KEY_R1==0); // wait for button release
-        }
-	   else if(KEY_R2 == 0)// && KEY_R0 == 1 && KEY_R1 == 1 && KEY_R3 == 1)
-        {
-         returnChar = '7';
-         while(KEY_R2==0); // wait for button release
-        }
-       else if(KEY_R3 == 0 && KEY_R0 == 1 && KEY_R1 == 1 && KEY_R2 == 1)
-        { 
-         returnChar = '*';
-         while(KEY_R3 ==0); // wait for button release
-        }
-      }
+		//If two or more buttons have not been pressed proceed to scan
+		if (rows < 2 && (col0 + col1 + col2) < 2){
+			//Reset the number of rows activated flag
+			rows = 0;
+			//for loop to scan the rows
+			for( row = 0; row < 4; row++){
 
-    
-     else if(column == 1) 
-       {
-       if(KEY_R0 == 0 && KEY_R1 ==1 && KEY_R2 == 1 && KEY_R3 == 1) 
-        {
-         returnChar = '2';
-         while(KEY_R0== 0); // wait for button release
-        }
-       else if(KEY_R1 == 0 && KEY_R0 == 1 && KEY_R2 == 1 && KEY_R3 ==1)
-        {
-         returnChar = '5';
-         while(KEY_R1==0); // wait for button release
-        }
-	   else if(KEY_R2 == 0 && KEY_R0 == 1 && KEY_R1 == 1 && KEY_R3 == 1)
-        {
-         returnChar = '8';
-         while(KEY_R2==0); // wait for button release
-        }
-      else if(KEY_R3 == 0 && KEY_R0 == 1 && KEY_R1 == 1 && KEY_R2 == 1)
-        { 
-         returnChar = '0';
-         while(KEY_R3 ==0); // wait for button release
-        }
-       }
 
-    
-      else if(column == 2) 
-        {
-       if(KEY_R0 == 0 && KEY_R1 ==1 && KEY_R2 == 1 && KEY_R3 == 1) 
-        {
-         returnChar = '3';
-         while(KEY_R0== 0); // wait for button release
-        }
-       else if(KEY_R1 == 0 && KEY_R0 == 1 && KEY_R2 == 1 && KEY_R3 ==1)
-        {
-         returnChar = '6';
-         while(KEY_R1==0); // wait for button release
-        }
-	   else if(KEY_R2 == 0 && KEY_R0 == 1 && KEY_R1 == 1 && KEY_R3 == 1)
-        {
-         returnChar = '9';
-         while(KEY_R2==0); // wait for button release
-        }
-       else if(KEY_R3 == 0 && KEY_R0 == 1 && KEY_R1 == 1 && KEY_R2 == 1)
-        { 
-         returnChar = '#';
-         while(KEY_R3 ==0); // wait for button release
-        }
-       }
+         if(row == 0)
+          {
+           KEY_R0 = 0;
+	       KEY_R1 = 1;
+           KEY_R2 = 1;
+           KEY_R3 = 1;
+          }
+         else if(row == 1)
+          {
+           KEY_R0 = 1;
+	       KEY_R1 = 0;
+           KEY_R2 = 1;
+           KEY_R3 = 1;
+          }
+         else if(row == 2)
+          {
+           KEY_R0 = 1;
+	       KEY_R1 = 1;
+           KEY_R2 = 0;
+           KEY_R3 = 1;
+          }
 
-      else
-        {
-         returnChar = -1; // either no button pressed or buttons simulataneously pressed
-         while(KEY_R0 == 0 || KEY_R1 == 0 || KEY_R2 == 0 || KEY_R3 == 0); // wait for button(s) to be released
-        } 
-     
-     }   
-return returnChar;
+         else if(row == 3)
+          {
+           KEY_R0 = 1;
+	       KEY_R1 = 1;
+           KEY_R2 = 1;
+           KEY_R3 = 0;
+          }
+           
 
- }
+				//Use Bit masking and shifting to set each row to zero for each pass of the for loop
+	//			ROWB |= 0x0C00; 	//Clear RB11->RB10
+     //           ROWA |= 0x0003;     //Clear RA1->RA0
+				
+
+            //    ROWB &= 0xF7FF; 	//Set RB11->RB10 to '0111'
+             //   ROWA &= 0xFFFD;     //Set RA1->RA0 to '1101
+				
+            //    ROWB ^= 0x0C00; 	//Invert RB11->RB10 for shifting
+            //    ROWA ^= 0x0003;     //Invert RA1->RA0 for shifting
+				
+            //    ROWB >>= row;		//Shift by row number
+            //    ROWA >>= row;
+				
+     //           ROWB ^= 0x0C00;	//Invert back to set correct ground
+       //         ROWA ^= 0x0003;   
+
+
+
+
+
+			
+	            DelayUs(10); //Delay to allow signal response
+
+				//Check if a button in pressed for this row iteration
+				if((KEY_C0 == 0) || (KEY_C1 == 0) || (KEY_C2 == 0)){
+					
+					rows++; //If a pressed button is detected increase number of rows activated
+
+					//Row 0 through 2 check which column is pressed and update the key character
+					if (row < 3){
+						if (KEY_C0 == 0){
+							key = 0x31 + row * 3;
+						}
+						if (KEY_C1 == 0){ 
+							key = 0x32 + row * 3;
+						}	
+						if (KEY_C2 == 0){ 
+							key = 0x33 + row * 3;	
+						}
+					}
+					//	If row is 3 check rows and set custom characters that don't fit above method
+					else if (row == 3){
+						if (KEY_C0 == 0){
+							key = 0x2A; 		//'*'
+						}
+						if (KEY_C1 == 0){
+							key = 0x30;			//'0'
+						}
+						if (KEY_C2 == 0){
+							key = 0x23;			//'#'
+						}
+					}
+				}
+			}	
+		}
+                // Ground all rows
+               // ROWB &= 0xF3FF; // RB11 & RB10 = 0
+               // ROWA &= 0xFFFC; // RA0 & RA1 = 0
+
+// Ground all the rows
+ KEY_R0 = 0;
+ KEY_R1 = 0;
+ KEY_R2 = 0;
+ KEY_R3 = 0;
+
+		DelayUs(10); //Delay to allow signal response
+	}
+
+	//If the sum of rows and columns activated is more than 2, it means more than one button has been pressed.
+	if ((col0 + col1 + col2 + rows) > 2) return -1; 
+	else return key;
+}
+
+// ******************************************************************************************* //
